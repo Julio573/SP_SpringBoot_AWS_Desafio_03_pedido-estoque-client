5b78f983e7c41938b0b253592956fc7186564b5f
Julio Peixoto <julio8497@hotmail.com>
2024-11-27 13:45:01 -0300

    updating project

---
+++ inventory/src/main/java/entities/Product.java
@@ -0,0 +1,31 @@
+package entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "product")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer amountInStock;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+}
