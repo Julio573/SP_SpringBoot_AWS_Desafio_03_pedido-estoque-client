0c155e6cfec927c9e2f170d211432e8d7ff4e6b5
Julio Peixoto <julio8497@hotmail.com>
2024-11-27 20:41:48 -0300

    Client entitie

---
+++ client/src/main/java/entities/Client.java
@@ -0,0 +1,30 @@
+package entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "clients")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_usuario")
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true, length = 100)
+    private String username;
+
+    @Column(name = "password", nullable = false, length = 200)
+    private String password;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+}
